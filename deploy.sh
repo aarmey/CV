a4bc9183bbe8ff3a925f6280173c7830551f5d31
Aaron Meyer <a@ameyer.me>
2017-04-13 10:54:41 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ git init
 git config user.name "Jenkins CI"
 git config user.email "jenkins@asmlab.org"
 
-git remote add upstream "git@github.com:thanatosmin/CV.git" -v
+git remote add upstream "git@github.com:thanatosmin/CV.git"
 git fetch upstream
 git reset upstream/gh-pages
 
@@ -19,4 +19,7 @@ touch .
 git add -A .
 git status
 git commit -m "Lastest site built on successful jenkins build $BUILD_NUMBER auto-pushed to github"
+
+echo "Pushing now."
+
 git push -q upstream HEAD:gh-pages
